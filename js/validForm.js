20697f317ea71fc301db2cc0d102e1ac30c8500b
thomas796 <perrierthomas9@gmail.com>
2020-10-28 12:24:51 +0100

    css form

--- js/validForm.js
+++ js/validForm.js
@@ -12,11 +12,11 @@ const validForm = () => {
             //Création de l'objet à envoyer
             const contact = inputCheck.contact
             const products = cartCheck.products
+        
             let objet = {
                 contact,
                 products
             }
-
             sendData(objet)
 
         } else {
@@ -33,58 +33,21 @@ const sendData = (objetRequest) => {
     var data = new FormData();
     data.append( "json", JSON.stringify( objetRequest ) );
     
-    fetch("http://localhost:3000/api/furniture/order",
-    {
+    fetch("http://localhost:3000/api/furniture/order", {
         method: "POST",
         body: data
     })
-    .then(function(res) { 
+    .then(function(res) {
+        console.log(res)
         return res.json()
     })
-    .then(function(data) { 
+    .then(function(data) {
         console.log( JSON.stringify( data ) ) 
     }).catch(error => {
         console.log('mon erreur' + error)
     })
 
 }
-
-  //Fonction requet post de l'API
-  const sendDatar = (objetRequest) => {
-    console.log('sendData')
-    console.log(objetRequest)
-
-    return new Promise((resolve)=>{
-        let request = new XMLHttpRequest();
-        request.onreadystatechange = function() {
-            // if(this.readyState == XMLHttpRequest.DONE && this.status == 201) {
-            if(this.readyState == XMLHttpRequest.DONE) {
-                console.log('ready')
-                console.log(this.responseText)
-
-                // sessionStorage.setItem("order", this.responseText);
-                //Chargement de la page de confirmation puis chargement de resultOrder
-                // document.forms["form-panier"].action = './order-confirm.html'; 
-                // document.forms["form-panier"].submit();
-
-                resolve(JSON.parse(this.responseText));
-
-                console.log(resolve)
-
-               // localStorage.clear()
-
-            }
-        }
-
-        request.open("POST", "http://localhost:3000/api/furniture/order");
-        request.setRequestHeader("Content-Type", "application/json");
-        request.send(objetRequest);
-    })
-
-}
-
-
-
 
 
 
@@ -119,9 +82,9 @@ const checkInput = () => {
     let contact = {
         firstName : firstName,
         lastName : lastname,
-        email : mail,
         address : adress,
-        district : district
+        district : district,
+        email : mail
     }
 
     return {contact: contact, errorMessage: errorMessage}
@@ -133,8 +96,9 @@ const checkInput = () => {
 const checkCart = () => {
 
     let idProductJson = localStorage.getItem("stockageCart");
-    let products = JSON.parse(idProductJson);
-
+    let products = []
+    products = JSON.parse(idProductJson);
+    console.log(products)
     let errorMessage = null
 
     if (idProductJson == null) {
